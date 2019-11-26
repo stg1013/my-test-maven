0041ba9856ec4fd880b181228f7f0abcfd8c43b4
binnetwork <lslh0989@163.com>
2019-11-26 22:39:10 +0800

    我是辉少

--- src/main/java/com/zuxia/test/Test.java
+++ src/main/java/com/zuxia/test/Test.java
@@ -1,7 +1,11 @@
 package com.zuxia.test;
 
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("asdfakdfja");
+        System.out.println("hello,world");
+
+
     }
 }
