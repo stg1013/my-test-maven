07b5fee849ece34e1fd4c6714e9f5aeb65d1d033
e <2126790087@qq.com>
2019-11-26 20:26:02 +0800

    首次创建

---
+++ src/test/java/com/zuxia/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuxia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
